87ab20df6c17070a913f17fc05cd735e11fe2bbe
yelo-o <flyordig@gmail.com>
2023-10-07 14:16:44 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int len = message.length();
+        answer = len * 2;
+        return answer;
+    }
+}
