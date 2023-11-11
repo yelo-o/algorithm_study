ad6ebe8ce9e782df04594640d415ed16798c8785
yelo-o <flyordig@gmail.com>
2023-11-11 12:43:13 +0900

    [level unrated] Title: 커피 심부름, Time: 1.09 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -4,16 +4,9 @@ class Solution {
     public int solution(String[] order) {
         int answer = 0;
         List<Integer> priceList = Arrays.stream(order)
-                .map(i -> {
-                            if (i.contains("latte")) {
-                                return 5000;
-                            } else {
-                                return 4500;
-                            }
-                        }
-                )
+                .map(i -> i.contains("latte")? 5000:4500)
                 .collect(Collectors.toList());
-//        System.out.println(priceList);
+
         for (Integer i : priceList) {
             answer += i;
         }
