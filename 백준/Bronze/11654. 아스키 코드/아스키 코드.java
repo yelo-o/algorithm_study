f423612818f17c959a3ce10cdd769a8e7b0a92fb
yelo-o <flyordig@gmail.com>
2023-12-04 13:41:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        char c = s.charAt(0);
+        System.out.println((int) c);
+
+    }
+
+}
