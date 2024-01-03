7eec9104c4578fc4456be6a44616abd89e9d8f19
yelo-o <flyordig@gmail.com>
2024-01-03 12:19:18 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int answer = factorial(n) / (factorial(k) * factorial(n - k)); // 조합 nCk = n! / { k! * (n - k)! } 
+        System.out.println(answer);
+    }
+
+    private static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
